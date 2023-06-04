ca2638281ac2016d2a0d4b008765718ae1fb039d
Mohammed Kenaway <70346420+mohammedgamal2002@users.noreply.github.com>
2023-06-04 12:58:32 +0300

    Add files via upload

---
+++ neural_networks.py
@@ -0,0 +1,88 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Load the Iris dataset
+iris = load_iris()
+
+# col_names = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width','Class']
+
+X = iris.data
+y = iris.target
+
+# Perform label encoding on the target variable
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Standardize the feature data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Define the model architecture
+model = keras.Sequential([
+    keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(3, activation='softmax')
+])
+
+optimizer = keras.optimizers.Adam(learning_rate=0.002)
+# Compile the model
+model.compile(optimizer= optimizer,
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+history = model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_test, y_test))
+
+# Plot the training and validation accuracy over epochs
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+# Plot the training and validation loss over epochs
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+# Evaluate the model
+test_loss, test_accuracy = model.evaluate(X_test, y_test)
+print('Test loss:', test_loss)
+print('Test accuracy:', test_accuracy)
+
+
+
+# Generate additional random test data
+num_samples = 50
+random_test_data = np.random.rand(num_samples, 4) * 8.0
+scaled_random_test_data = scaler.transform(random_test_data)
+
+# Make predictions on the additional test data
+predictions = model.predict(scaled_random_test_data)
+predicted_labels = np.argmax(predictions, axis=1)
+predicted_species = label_encoder.inverse_transform(predicted_labels)
+
+predicted_species_names = [iris.target_names[label] for label in predicted_species]
+
+# Print the predicted species for the additional test data
+print('Predicted species for additional test data:')
+for i in range(num_samples):
+    print(f'Test sample {i+1}: {predicted_species_names[i]}')
+
+
